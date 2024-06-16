e7f31089a95b9495330aca41612cf901ed540735
Nuraisyahaswari58 <142501173+Nuraisyahaswari58@users.noreply.github.com>
2024-06-17 00:42:39 +0700

    Delete config directory old

--- config/MachineLearning.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-const url_homepage = process.env.ML_URL_homepage;
-const url_survey = process.env.ML_URL_survey;
-
-export default {url_homepage, url_survey};
